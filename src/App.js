5ea29098f355f8f463661f6e8b45580d18548fe4
TamannaAfroz2666 <afroztamanna66@gmail.com>
2022-12-26 11:55:13 +0600

    add navbar

--- src/App.js
+++ src/App.js
@@ -2,13 +2,46 @@
 import './App.css';
 import '../node_modules/bootstrap/dist/css/bootstrap.css'
 import 'bootstrap/dist/css/bootstrap.min.css';
-// import HomePage from './components/HomePage';
+import { BrowserRouter as Router, Route, Link } from 'react-router-dom';
+import { Input  } from 'semantic-ui-react'
+import HomePage from './components/HomePage';
 
 function App() {
   return (
+    <Router>
     <div className="App">
-  
+    <div className=''>
+              <div class="ui secondary menu parent-div">
+              <Link to='/' className='navbar item'>
+                Login
+              </Link>
+
+              <Link to='/home' className=' navbar item '>
+              Home 
+              </Link>
+              <Link to='/about' className='navbar item '>
+              About 
+              </Link>
+              <Link to='/products' className=' navbar item'>
+              Product
+              </Link>                
+                <div class="right menu">
+                  <div class=" item">
+                    <div class="ui icon input">
+                      <Input type="text" placeholder="Search..." />
+                      <i class="search link icon"></i>
+                </div>
+            </div>
+            <Link to='/logout' className=' navbar item'>
+              Logout
+              </Link>
+          </div>
+        </div>
+          </div>
+          <h1 className='text-center'>hello world</h1>
+      <Route path={'/home'} component={HomePage} exact/>
     </div>
+    </Router>
   );
 }
 
